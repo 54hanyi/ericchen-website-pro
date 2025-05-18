d6a8ef8dcaf96df8f9f2c4ee3b8c0278f890b47e
陳翰毅 <tp6c04u4456@yahoo.com.tw>
2025-05-18 23:26:05 +0800

    chore: 移動components資料夾從app至根目錄

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { ReactNode } from 'react'
-import NavBar from './components/Navbar'
-import Footer from './components/Footer'
+import NavBar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export const metadata = {
   title: 'Eric 技術展示網站',
